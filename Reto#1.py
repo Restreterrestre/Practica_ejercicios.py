1063e5da073495400f7ed8cc602ef0fa74ebea5a
Juan Pablo <juan.perezep@amigo.edu.co>
2025-06-19 09:31:02 -0500

    Este es la primera parte de los ejercicios de python

---
+++ Reto#1.py
@@ -0,0 +1,22 @@
+#Este es un comentario de una sola linea 
+#https://www.python.org/
+
+'''
+Este es un 
+comentario multi 
+linea
+'''
+
+Variable_Constante =  3
+
+variable = "  "
+
+Almacenador = 0
+
+Caracter = "Carlos"
+Numero = 5
+Flotante = 1.23123
+Boleano = True
+Boleano_falso = False
+
+print("Hola mundo","Pyt" "hon")
---
+++ Reto#2.py
@@ -0,0 +1,63 @@
+#Este es el reto numero dos de ejercicios 
+
+#Operadores
+
+#Operadores aritmeticos
+
+print("Suma:", 2+2)
+print("Resta:", 4-2)
+print("Multiplicacion:", 2*2)
+print("Divicion:",10/3)
+print("Modulo",10 % 3) #Devuelve lo que sobra despues de realizar la divicion entera
+print("Potencia", 4**3)
+print("Diviso con numero entero:",17//4)
+
+#Operadores relacionales 
+
+print("2 Mayor que 4", 2 > 4)
+print("4 Mayor que 2", 4 > 2)
+print(" 4 Menor que 2", 4 < 2)
+print("2 Menor que 4", 2 < 4)
+print("2 es igual 2", 2 == 2)
+print("2 es igual a 5", 2 == 5)
+print("Mayor igaul", 5 >= 7)
+print("Menor igual ", 5 <= 7)
+print("No son iguale 2 y 3", 2 != 3) #Mostrara verdadero si no son iguales en caso de ser iguales mostrara falso 
+
+#Operadores de asignacion 
+
+Variable = 1
+print(Variable)
+Variable = "Cambio a texto"
+print(Variable)
+Numero = 5
+Numero += 5 
+print(Numero)
+Numero -= 3
+print(Numero)
+Numero *= 2
+print(Numero)
+Numero %= 3
+print(Numero)
+Numero /= 2
+print(Numero)
+Numero += 2
+Numero **= 2
+print(Numero)
+Numero //= 2
+print(Numero)
+#Numero &= 2
+#print(Numero)
+#Numero |= 2
+#print(Numero)
+#Numero ^= 2
+#print(Numero)
+ 
+Nuevo_numero = 2
+Nuevo_numero <<= 2
+print(Nuevo_numero)
+
+
+
+
+
